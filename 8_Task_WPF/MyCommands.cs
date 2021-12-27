9a1c89b1dd16db091558a22905f6257027691a55
Администратор <@DESKTOP-PJPICF5>
2021-12-27 19:52:31 +0800

    Добавьте файлы проекта.

---
+++ 8_Task_WPF/MyCommands.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace _8_Task_WPF
+{
+    class MyCommands
+    {
+        public static RoutedUICommand Exit { get; set; }
+        static MyCommands()
+        {
+            InputGestureCollection inputs = new InputGestureCollection();
+            inputs.Add(new KeyGesture(Key.F4, ModifierKeys.Alt, "Alt+F4"));
+            Exit = new RoutedUICommand("Exit", "Exit", typeof(MyCommands), inputs);
+        }
+    }
+}
